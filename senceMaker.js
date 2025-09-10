ed34e44abf693ef9b17a332b720af51946127364
xaimohchau@gmail.com <xaimoh@gmail.com>
2025-09-10 20:33:10 +0300

    first practice files

---
+++ senceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "great";
+let noun = "giant";
+let verb = "breating";
+let place = "cave";
+let adjective2 = "big";
+let noun2 = "meat";
+
+let firstStory = "Once upon a time, there was a " + adjective + " "+ noun + " who loved to eat " + noun2 + "." + " The " + noun  + " lived in a " + place + " and had " + adjective2 + " nostlings that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "scarery";
+noun = "python";
+verb = "furious";
+place = "forest";
+adjective2 = "ugly";
+noun2 = "rodents";
+
+let secondStory = "Once upon a time, there was a " + adjective + " "+ noun + " who loved to eat " + noun2 + "." + " The " + noun  + " lived in a " + place + " and had " + adjective2 + " nostlings that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
